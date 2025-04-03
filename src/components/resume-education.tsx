d5cfdba70c78b6bfd945e4e033394ed570958d12
xieyezi <1435398529@qq.com>
2025-04-03 22:54:50 +0800

    refactor: update ResumeEducation and ResumeProfile components with new data and styling adjustments

--- src/components/resume-education.tsx
+++ src/components/resume-education.tsx
@@ -1,49 +1,29 @@
 export function ResumeEducation() {
   return (
-    <div className="content-section">
-      <div className="content-section-holder mt-3">
-        <header className="section-title-header uppercase tracking-wide">
-          <h2 className="text-base mb-1 text-gray-800 dark:text-white">教育经历</h2>
+    <div className='content-section'>
+      <div className='content-section-holder mt-3'>
+        <header className='section-title-header uppercase tracking-wide'>
+          <h2 className='text-base mb-1 text-gray-800 dark:text-white'>教育经历</h2>
         </header>
 
-        <div className="content-section-container md:grid md:grid-cols-2 md:gap-6">
+        <div className='content-section-container'>
           {/* First Education */}
-          <div className="section-detail-item mt-3 border-b border-gray-200 pb-2 border-dashed dark:border-gray-700">
-            <div className="normal-content">
-              <div className="grid md:grid-cols-5">
-                <div className="md:col-span-1 left-time mt-0 mb-2 mr-0 md:mr-4">
-                  <h4 className="md:text-left text-gray-400 flex justify-between md:block dark:opacity-80">
-                    <div>2010/09 - 2012/06</div>
+          <div className='section-detail-item mt-3 border-b border-gray-200 pb-2 border-dashed dark:border-gray-700'>
+            <div className='normal-content'>
+              <div className='grid md:grid-cols-5'>
+                <div className='md:col-span-1 left-time mt-0 mb-2 mr-0 md:mr-4'>
+                  <h4 className='md:text-left text-gray-400 flex justify-between md:block dark:opacity-80'>
+                    <div>2015/09 - 2019/06</div>
                   </h4>
                 </div>
 
-                <div className="md:col-span-4">
+                <div className='md:col-span-4'>
                   <header>
-                    <h3 className="font-semibold mb-2 dark:text-white">硕士</h3>
-                    <div className="flex justify-between flex-wrap mb-2 text-gray-400 dark:opacity-80">
-                      <h4>清华大学 <span className="ml-2">软件工程</span></h4>
-                    </div>
-                  </header>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* Second Education */}
-          <div className="section-detail-item mt-3 border-b border-gray-200 pb-2 border-dashed dark:border-gray-700">
-            <div className="normal-content">
-              <div className="grid md:grid-cols-5">
-                <div className="md:col-span-1 left-time mt-0 mb-2 mr-0 md:mr-4">
-                  <h4 className="md:text-left text-gray-400 flex justify-between md:block dark:opacity-80">
-                    <div>2006/09 - 2010/07</div>
-                  </h4>
-                </div>
-
-                <div className="md:col-span-4">
-                  <header>
-                    <h3 className="font-semibold mb-2 dark:text-white">学士</h3>
-                    <div className="flex justify-between flex-wrap mb-2 text-gray-400 dark:opacity-80">
-                      <h4>湖南大学 <span className="ml-2">计算机科学与技术</span></h4>
+                    <h3 className='font-semibold mb-2 dark:text-white'>学士</h3>
+                    <div className='flex justify-between flex-wrap mb-2 text-gray-400 dark:opacity-80'>
+                      <h4>
+                        重庆理工大学 <span className='ml-2'>计算机科学与技术</span>
+                      </h4>
                     </div>
                   </header>
                 </div>
--- src/components/resume-profile.tsx
+++ src/components/resume-profile.tsx
@@ -1,46 +1,54 @@
 import Image from "next/image";
-import Link from "next/link";
-import { Mail, Phone, Globe, Github, Linkedin } from "lucide-react";
+import { Mail, Phone, Globe, Github, MessageCircleMore } from "lucide-react";
+import Profile from "@/static/avator.png";
 
 export function ResumeProfile() {
   return (
-    <header className="resume-header md:flex md:items-center md:justify-between md:flex-row-reverse mb-4">
-      <div className="relative w-28 h-28 rounded-full overflow-hidden mx-auto md:mx-0">
-        <Image
-          src="https://ext.same-assets.com/3652286615/171585982.png"
-          alt="Profile"
-          fill
-          className="object-cover"
-        />
+    <header className='resume-header md:flex md:items-center md:justify-between md:flex-row-reverse mb-4'>
+      <div className='relative w-28 h-28 rounded-full overflow-hidden mx-auto md:mx-0'>
+        <Image src={Profile} alt='Profile' fill className='object-cover' />
       </div>
 
-      <div className="header-info md:text-left text-center mt-2 md:mt-0">
-        <h1 className="text-xl mb-2 text-gray-800 dark:text-white font-semibold">马超云</h1>
-        <h2 className="mb-2 text-gray-600 dark:text-gray-300">高级前端开发工程师 上海</h2>
+      <div className='header-info md:text-left text-center mt-2 md:mt-0'>
+        <h1 className='text-xl mb-2 text-gray-800 dark:text-white font-semibold'>苏业纯</h1>
+        <h2 className='mb-2 text-gray-600 dark:text-gray-300'>高级前端工程师 重庆</h2>
 
-        <ul className="link-list text-left flex md:items-center flex-wrap">
-          <li className="mr-3 mb-2 flex items-center">
-            <Mail className="w-4 h-4 mr-1 text-gray-500" />
-            <a href="mailto:hey@easycv.cn" className="text-gray-600 dark:text-gray-300 hover:text-gray-800">hey@easycv.cn</a>
+        <ul className='link-list text-left flex md:items-center flex-wrap'>
+          <li className='mr-3 mb-2 flex items-center'>
+            <Mail className='w-4 h-4 mr-1 text-gray-500' />
+            <a href='mailto:1435398529@qq.com' className='text-gray-600 dark:text-gray-300 hover:text-gray-800'>
+              1435398529@qq.com
+            </a>
           </li>
-          <li className="mr-3 mb-2 flex items-center">
-            <Phone className="w-4 h-4 mr-1 text-gray-500" />
-            <span className="text-gray-600 dark:text-gray-300">13611111234</span>
+          <li className='mr-3 mb-2 flex items-center'>
+            <Phone className='w-4 h-4 mr-1 text-gray-500' />
+            <span className='text-gray-600 dark:text-gray-300'>17783887443</span>
           </li>
         </ul>
 
-        <ul className="link-list text-left flex md:items-center flex-wrap">
-          <li className="mr-3 mb-2 flex items-center">
-            <Globe className="w-4 h-4 mr-1 text-gray-500" />
-            <a href="https://vikingz.me" target="_blank" className="text-gray-600 dark:text-gray-300 hover:text-gray-800" rel="noreferrer">个人网站</a>
+        <ul className='link-list text-left flex md:items-center flex-wrap'>
+          <li className='mr-3 mb-2 flex items-center'>
+            <MessageCircleMore className='w-4 h-4 mr-1 text-gray-500' />
+            <a
+              href='https://www.helloimg.com/i/2025/04/03/67ee9eb6239ba.png'
+              target='_blank'
+              className='text-gray-600 dark:text-gray-300 hover:text-gray-800'
+              rel='noreferrer'
+            >
+              xieyezi-syc
+            </a>
           </li>
-          <li className="mr-3 mb-2 flex items-center">
-            <Github className="w-4 h-4 mr-1 text-gray-500" />
-            <a href="https://github.com/vikingmute" target="_blank" className="text-gray-600 dark:text-gray-300 hover:text-gray-800" rel="noreferrer">Github</a>
+          <li className='mr-3 mb-2 flex items-center'>
+            <Github className='w-4 h-4 mr-1 text-gray-500' />
+            <a href='https://github.com/xieyezi' target='_blank' className='text-gray-600 dark:text-gray-300 hover:text-gray-800' rel='noreferrer'>
+              Github
+            </a>
           </li>
-          <li className="mr-3 mb-2 flex items-center">
-            <Linkedin className="w-4 h-4 mr-1 text-gray-500" />
-            <a href="https://linkedin.com/vikingmute" target="_blank" className="text-gray-600 dark:text-gray-300 hover:text-gray-800" rel="noreferrer">LinkedIn</a>
+          <li className='mr-3 mb-2 flex items-center'>
+            <Globe className='w-4 h-4 mr-1 text-gray-500' />
+            <a href='https://me.xieyezi.us:5003/' target='_blank' className='text-gray-600 dark:text-gray-300 hover:text-gray-800' rel='noreferrer'>
+              个人网站
+            </a>
           </li>
         </ul>
       </div>
