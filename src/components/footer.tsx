410584b395aceded244e937ecac78d51188b2e07
xieyezi <1435398529@qq.com>
2025-12-05 17:43:28 +0800

    refactor: update Footer component layout for improved readability and consistency in translations

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,19 +1,23 @@
 "use client";
 
 import Link from "next/link";
-import { useTranslations } from 'next-intl';
+import { useTranslations } from "next-intl";
 
 export function Footer() {
-  const t = useTranslations('footer');
+  const t = useTranslations("footer");
 
   return (
     <footer className='container mx-auto pt-4 pb-6 px-12 flex flex-wrap flex-col sm:flex-row max-w-screen-lg text-gray-500 text-sm leading-5 items-center'>
-      <p className='text-center sm:text-left'>
-        {t('copyright')}
-        <Link href='https://github.com/xieyezi' target='_blank' className='hover:underline'>
-          💗 xieyezi
-        </Link>
-      </p>
+      <div className='flex items-center gap-2 text-center sm:text-left'>
+        <span>
+          {t("copyright")}
+          <Link href='https://github.com/xieyezi' target='_blank' className='hover:underline'>
+            💗 xieyezi
+          </Link>
+        </span>
+        <span className='text-gray-400'>|</span>
+        <span>build with next.js</span>
+      </div>
     </footer>
   );
 }
